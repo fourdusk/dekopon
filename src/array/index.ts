2760ba5737baccfbb459ac3ed18bfdc538d34d12
Javion <javionlog@163.com>
2024-05-23 14:24:17 +0800

    feat(array): add listToTree

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,10 +1,10 @@
 import type { BaseRecord } from '#/index'
 
 export const treeToList = <T extends BaseRecord>(
-  tree: T[],
-  childrenKey = 'children',
-  isDepthFirst = true
+  tree: T[] = [],
+  props = { childrenKey: 'children', isDepthFirst: true }
 ) => {
+  const { childrenKey, isDepthFirst } = props
   const stack = tree.slice()
   const result: T[] = []
   while (stack.length > 0) {
@@ -21,5 +21,46 @@ export const treeToList = <T extends BaseRecord>(
       }
     }
   }
+  return result
+}
+
+export const listToTree = <
+  T extends {
+    [key: string | number]: unknown
+  }
+>(
+  list: T[] = [],
+  props = { parentId: 'parentId', childrenId: 'id', childrenKey: 'children' }
+) => {
+  const { parentId, childrenId, childrenKey } = props
+  const result = [] as (
+    | T
+    | {
+        [K in typeof childrenKey]: T
+      }
+  )[]
+  const pIdMap: {
+    [key: number | string]: T
+  } = {}
+
+  for (const item of list) {
+    pIdMap[item[childrenId] as string | number] = item
+  }
+
+  for (const item of list) {
+    if (item[parentId]) {
+      const mapIem = pIdMap[item[parentId] as string | number] as {
+        [key: number | string]: T[]
+      }
+      if (Array.isArray(mapIem[childrenKey])) {
+        mapIem[childrenKey].push(pIdMap[item[childrenId] as string | number])
+      } else {
+        mapIem[childrenKey] = [pIdMap[item[childrenId] as string | number]]
+      }
+    } else {
+      result.push(pIdMap[item[childrenId] as string | number])
+    }
+  }
+
   return result
 }
